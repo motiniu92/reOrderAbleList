1aef837c10e9768c5c686ae743af17607d8254a1
motiniu92 <motiniu92@gmail.com>
2025-07-01 17:33:26 +0600

    add handle location permission

--- lib/googleMapLiveLocation/direction_map_screen.dart
+++ lib/googleMapLiveLocation/direction_map_screen.dart
@@ -1,12 +1,12 @@
 import 'dart:convert';
+import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:http/http.dart' as http;
-import 'package:geocoding/geocoding.dart';
-import 'package:maps_launcher/maps_launcher.dart';
+import 'package:mime/mime.dart';
 import 'package:screenshot/screenshot.dart';
 
 class DirectionMapScreen extends StatefulWidget {
@@ -19,6 +19,7 @@ class _DirectionMapScreenState extends State<DirectionMapScreen> {
   ScreenshotController screenshotController = ScreenshotController();
   Uint8List? _imageFile; // Make it nullable
   String? _base64Image;
+  File? file;
 
   final LatLng _startPoint = LatLng(23.7323, 90.4145); // Paltan Bus Stop
   final LatLng _endPoint = LatLng(23.7285, 90.4106); // Shapla Chattar
@@ -29,6 +30,8 @@ class _DirectionMapScreenState extends State<DirectionMapScreen> {
   @override
   void initState() {
     super.initState();
+    _handleLocationPermission();
+    getETAByBus(_startPoint, _endPoint);
     _setPolyline();
   }
 
@@ -64,11 +67,14 @@ class _DirectionMapScreenState extends State<DirectionMapScreen> {
   void _captureScreenshot() async {
     final image = await mapController.takeSnapshot();
     if (image != null) {
+      final mimeType = getMimeTypeFromBytes(image);
+      final base64Str = base64Encode(image);
+      final base64Image = 'data:$mimeType;base64,$base64Str';
+
       setState(() {
         _imageFile = image;
-        _base64Image = base64Encode(image);
+        _base64Image = base64Image;
       });
-
       print("Screenshot captured...: ${_base64Image})");
 
       // Optional: calculate distance
@@ -86,30 +92,88 @@ class _DirectionMapScreenState extends State<DirectionMapScreen> {
     }
   }
 
+  Future<void> _handleLocationPermission() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        print('Location permission denied');
+        return;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      print('Location permission permanently denied');
+      return;
+    }
+
+    print('Location permission granted');
+  }
+
+  Future<void> getETAByBus(LatLng start, LatLng end) async {
+    const String apiKey = 'AIzaSyC3NpcTc3Wdtn68VCsf7ZxswuDzlNvvt9c';
+
+    print("Api Key: $apiKey");
+    final url = Uri.parse(
+      'https://maps.googleapis.com/maps/api/directions/json'
+      '?origin=${start.latitude},${start.longitude}'
+      '&destination=${end.latitude},${end.longitude}'
+      '&mode=transit'
+      '&transit_mode=bus'
+      '&key=$apiKey',
+    );
+    print("Google Map Url...: $url");
+    try {
+      final response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+
+        if (data['routes'] != null && data['routes'].isNotEmpty) {
+          final duration = data['routes'][0]['legs'][0]['duration']['text'];
+          print("ETA by Bus: $duration");
+        } else {
+          print("No routes found.");
+        }
+      } else {
+        print("Failed to get directions: ${response.statusCode}");
+      }
+    } catch (e) {
+      print("Error fetching ETA by bus: $e");
+    }
+  }
+
+  String getMimeTypeFromBytes(Uint8List bytes) {
+    return lookupMimeType('', headerBytes: bytes) ?? 'image/png';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Google Map Screenshot')),
-      body: Column(
-        children: [
-          Expanded(
-            child: GoogleMap(
-              onMapCreated: (controller) {
-                _onMapCreated(controller);
-                Future.delayed(Duration(seconds: 2),
-                    _captureScreenshot); // wait to render fully
-              },
-              initialCameraPosition: CameraPosition(
-                target: _startPoint,
-                zoom: 15.0,
-              ),
-              polylines: _polylines,
-              markers: _markers,
+      body: (_startPoint == null || _endPoint == null)
+          ? Center(child: CircularProgressIndicator())
+          : Column(
+              children: [
+                Expanded(
+                  child: GoogleMap(
+                    onMapCreated: (controller) {
+                      _onMapCreated(controller);
+                      Future.delayed(Duration(seconds: 2),
+                          _captureScreenshot); // wait to render fully
+                    },
+                    initialCameraPosition: CameraPosition(
+                      target: _startPoint,
+                      zoom: 15.0,
+                    ),
+                    polylines: _polylines,
+                    markers: _markers,
+                  ),
+                ),
+                if (_imageFile != null) Image.memory(_imageFile!),
+              ],
             ),
-          ),
-          if (_imageFile != null) Image.memory(_imageFile!),
-        ],
-      ),
     );
   }
 }
